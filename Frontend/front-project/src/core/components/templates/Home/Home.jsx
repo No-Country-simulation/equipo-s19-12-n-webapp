a9f48bfe9501a2cd57fee1f47668c00745b9799d
MiltonOscarFlores <miltonoscarflores@gmail.com>
2024-11-22 06:20:48 -0300

    Integrados navbar main hero y footer a la estructura de carpetas elegida

--- Frontend/front-project/src/core/components/templates/Home/Home.jsx
+++ Frontend/front-project/src/core/components/templates/Home/Home.jsx
@@ -1,15 +1,25 @@
-import "./style.css";
-import Navbar from "../../organismos/Navbar/Navbar";
+// src/components/templates/Home/Home.jsx
+import { useState } from "react";
+import Layout from "../../../layouts/Layout.jsx"; // Importa el Layout
+import Hero from "../../organismos/Hero/Hero";
+import productsData from "../../../data/productsData.js";
 
-function Home() {
+const Home = () => {
+  const [selectedCategory, setSelectedCategory] = useState("frutas");
+
+  const handleCategorySelect = (category) => {
+    setSelectedCategory(category);
+  };
+
   return (
-    <>
-      <header>
-        <Navbar />
-      </header>
-      <hero>hero</hero>
-    </>
+    <Layout>
+      <Hero
+        selectedCategory={selectedCategory}
+        onCategorySelect={handleCategorySelect}
+        productsData={productsData}
+      />
+    </Layout>
   );
-}
+};
 
 export default Home;
