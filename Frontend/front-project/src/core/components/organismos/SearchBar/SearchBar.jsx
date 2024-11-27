23a0eb69b522986dd2c32eeb347caea01e50a3fd
MiltonOscarFlores <miltonoscarflores@gmail.com>
2024-11-27 07:05:13 -0300

    Feat: integrado el selector de categorias al SearchBar,faltan los estlos

--- Frontend/front-project/src/core/components/organismos/SearchBar/SearchBar.jsx
+++ Frontend/front-project/src/core/components/organismos/SearchBar/SearchBar.jsx
@@ -1,26 +1,46 @@
+import React, { useState } from "react";
 import SearchInput from "../../atomos/SearchInput/SearchInput";
 import Button from "../../atomos/Button/Button";
+import Selector from "../../atomos/Selector/Selector";
 import "./SearchBar.css";
 
 const SearchBar = () => {
+  const [selectedValue, setSelectedValue] = useState("");
+
+  // Definir las opciones para el selector
+  const options = [
+    { value: "", label: "Ordenar por" }, // Opción por defecto
+    { value: "precio", label: "Precio" },
+    { value: "popularidad", label: "Popularidad" },
+  ];
+
+  const handleSelectChange = (event) => {
+    const newValue = event.target.value;
+    setSelectedValue(newValue);
+  };
+
   return (
     <div className="search-bar">
       <div className="sectionSearch">
         <SearchInput />
       </div>
       <div className="sectionButtons">
-        <Button texto={"Ordenar por"} />
+        <Selector
+          options={options}
+          selectedValue={selectedValue}
+          handleChange={handleSelectChange}
+          borderType="rounded"
+        />
+
         <Button
-          texto={"Mapa"}
+          texto={"mapa"}
           variante={"white"}
           icon={"carrito"}
-          className="btns-search-bar"
         />
         <Button
           texto={"Carrito"}
           variante={"white"}
           icon={"carrito"}
-          className="btns-search-bar"
         />
       </div>
     </div>
