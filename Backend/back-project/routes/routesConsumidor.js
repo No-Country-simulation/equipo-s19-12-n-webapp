0925bbcada5b99a9d41806748d9833ef966e90d9
Zanchez <szwallie26@gmail.com>
2024-11-29 19:04:31 -0300

    feat: se agregaron a las rutas el manejo de errores

--- Backend/back-project/routes/routesConsumidor.js
+++ Backend/back-project/routes/routesConsumidor.js
@@ -4,33 +4,114 @@ import consumidorSchema from "../models/consumidor.model.js"
 const routerConsumidor = express.Router()
 
 routerConsumidor.get("/", async (req, res) => {
-    const consumidores = await consumidorSchema.find();
-    res.json(consumidores);
+    try {
+        const consumidores = await consumidorSchema.find();
+        return res.json(consumidores);
+    } catch (err) {
+        res.status(500).send({
+            message:
+                err.message || "Error al realizar la búsqueda"
+        });
+    }
 })
 
 routerConsumidor.get("/:email", async (req, res) => {
-    const consumidor = await consumidorSchema.findById(req.params.email);
-    res.json(consumidor);
+    try {
+        const data = await consumidorSchema.findById(req.params.email);
+
+        if (!data)
+            res.status(404).send({ message: "Registro no encontrado " });
+        else res.json(data);
+
+    } catch (err) {
+        res
+            .status(500)
+            .send({ message: "Error al buscar el registro con id = " + req.params.email });
+    };
 })
 
 routerConsumidor.post("/", async (req, res) => {
-    const {email, pass} = req.body;
-    const nuevoConsumidor = new consumidorSchema({email, pass});
-    await consumidorSchema.insertMany(nuevoConsumidor);
+    if (!req.body) {
+        return res.status(400).send({
+            message: "No se pueden enviar parámetros vacíos!"
+        });
+    }
+    try {
+        const {email, pass} = req.body;
+        const nuevoConsumidor = new consumidorSchema({email, pass});
+        await consumidorSchema.insertMany(nuevoConsumidor);
+        res.sendStatus(200).send({
+            message: "Se añadieron nuevos datos correctamente"
+        })
+    } catch (error) {
+        res.status(500).send({
+            message: "Error al añadir datos"
+        });
+    } 
 })
 
 routerConsumidor.post("/:email", async (req, res) => {
-    const {email, pass} = req.body;
-    const consumidorActualizado = new consumidorSchema({email, pass});
-    await consumidorSchema.findByIdAndUpdate(req.params.email, consumidorActualizado);
+
+    if (!req.body) {
+        return res.status(400).send({
+            message: "No se pueden enviar parámetros vacíos!"
+        });
+    }
+    try {
+        const {email, pass} = req.body;
+        const consumidorActualizado = new consumidorSchema({email, pass});
+        const data = await consumidorSchema.findByIdAndUpdate(req.params.email, consumidorActualizado);
+        if (!data) {
+            res.status(404).send({
+                message: `No es posible actualizar el producto con email = ${req.params.email}.`
+            });
+        } else res.send({ message: "Producto actualizado correctamente." });
+    }
+    catch (err) {
+        res.status(500).send({
+            message: "Error actualizando el producto con email = " + req.params.email
+        });
+    }
 })
 
 routerConsumidor.delete("/:email", async (req, res) => {
-    await consumidorSchema.findByIdAndDelete(req.params.email)
+    try {
+        const data = await consumidorSchema.findByIdAndDelete(req.params.email)
+
+        if (!data) {
+            res.status(404).send({
+                message: `No fue posible borrar el registro con el email = ${req.params.email}.`
+            });
+        } else {
+            res.send({
+                message: "El registro fue borrado correctamente!"
+            });
+        }
+    } catch (err) {
+        res.status(500).send({
+            message: "Error, no fue posible borrar el registro con el email = " + req.params.email
+        });
+    }
 })
 
 routerConsumidor.delete("/", async (req, res) => {
-    await consumidorSchema.remove();
+    try {
+        const data = await consumidorSchema.remove();
+
+        if (!data) {
+            res.status(404).send({
+                message: `No fue posible borrar los registros.`
+            });
+        } else {
+            res.send({
+                message: "Todos los registros has sido borrados correctamente!"
+            });
+        }
+    } catch (err) {
+        res.status(500).send({
+            message: "Error, no fue posible borrar los registros"
+        });
+    }
 })
 
 export default routerConsumidor;
