1f9f081af38e0232edcb64aa96c9fc7b7efd01fd
MiltonOscarFlores <miltonoscarflores@gmail.com>
2024-12-12 18:08:29 -0300

    Fix: completado la seccion Mision

--- Frontend/front-project/src/core/components/moleculas/CardMision/CardMision.jsx
+++ Frontend/front-project/src/core/components/moleculas/CardMision/CardMision.jsx
@@ -1,7 +1,19 @@
 import "./CardMision.css";
 
-const CardMision = () => {
-  return <div className="card-mision-container">Card</div>;
+const CardMision = ({ icon, title, description }) => {
+  return (
+    <div className="card-mision-container">
+      <div className="contenedor-img-card-mision">
+        <img
+          src={icon}
+          alt={title}
+          className="card-mision-icon"
+        />
+      </div>
+      <h3>{title}</h3>
+      <p>{description}</p>
+    </div>
+  );
 };
 
 export default CardMision;
--- Frontend/front-project/src/core/components/organismos/MainSection6/MainSection6.jsx
+++ Frontend/front-project/src/core/components/organismos/MainSection6/MainSection6.jsx
@@ -1,30 +1,32 @@
-import Texto from "../../atomos/Textos/Texto";
+import iconBasura from "../../../../assets/images/icono-verde-basura.svg";
+import iconImpacto from "../../../../assets/images/icono-verde-impacto.svg";
+import iconSolucion from "../../../../assets/images/icono-verde-solucion.svg";
 import CardMision from "../../moleculas/CardMision/CardMision";
-import "./MainSection6.css";
 
-const MainSection6 = () => {
+const Mision = () => {
   return (
-    <>
-      <div className="main6-contenedor">
-        <Texto
-          level={"h2"}
-          texto={"Nuestra misión"}
-          variante={"black"}
+    <div className="main6-contenedor">
+      <h2>Nuestra misión</h2>
+      <br />
+      <div className="main6-seccion-mision ">
+        <CardMision
+          icon={iconBasura}
+          title="Desperdicio de alimentos"
+          description="Cada año, se desperdician más de 1.3 mil millones de toneladas de alimentos comestibles a nivel mundial."
         />
-        <div className="main6-seccion-mision">
-          <CardMision>
-            <Texto
-              level={"h2"}
-              texto={"asaasa"}
-              variante={"black"}
-            />
-          </CardMision>
-          <CardMision />
-          <CardMision />
-        </div>
+        <CardMision
+          icon={iconImpacto}
+          title="Impacto ambiental"
+          description="Este problema tiene un enorme impacto negativo sobre el medio ambiente, contribuyendo a la emisión de gases de efecto invernadero y el deterioro de los ecosistemas."
+        />
+        <CardMision
+          icon={iconSolucion}
+          title="Nuestra solución"
+          description="En Eaty, buscamos conectar a los consumidores con productos alimenticios de calidad que de otro modo terminarían desechados, reduciendo el impacto ambiental y ofreciendo importantes ahorros."
+        />
       </div>
-    </>
+    </div>
   );
 };
 
-export default MainSection6;
+export default Mision;
